af0e5d93c5d4a10fc91f6d3cf1e6d0da73a5e107
Mahdi Abdollahi <mahdiabdollahi212@gmail.com>
2025-12-08 22:06:11 +0330

    app.py issue handled

--- app.py
+++ app.py
@@ -16,9 +16,21 @@ def recommend_api():
 
     try:
         result = recommend(title)
-        return jsonify({"input_movie": title, "recommendations": result.tolist()})
+
+        # تبدیل مطمئن به لیست
+        try:
+            result = list(result)
+        except:
+            result = result.tolist()
+
+        return jsonify({
+            "input_movie": title,
+            "recommendations": result
+        })
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=10000)
