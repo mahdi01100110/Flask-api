e3c34eeb429bcc41a8bb75e840268ab19150a8fa
Mahdi Abdollahi <mahdiabdollahi212@gmail.com>
2025-12-10 12:21:04 +0330

    coprs added to app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
 from MovieRecommendor import recommend  # تابع توصیه‌گر
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
